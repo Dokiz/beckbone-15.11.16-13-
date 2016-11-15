7e3f46906b7a92b5901835badcf0dfba935d45d8
Dokiz_228@mail.ru <deoptgqqr12>
2016-11-15 17:25:38 +0300

    start

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ $(function(){
 		Collections:{},
 	    Views:{} 
 		};
+		
 	window.template = function(id){
 		return _.template( $('#' + id).html() );
 		};
 
 		App.Models.Task = Backbone.Model.extend({});
+		
 		App.Views.Task = Backbone.View.extend({
 			tagName: 'li',
 			render: function (){
@@ -19,15 +21,45 @@ $(function(){
 			
     });
 	
-	 var task = new App.Models.Task({
+	App.Collections.Task = Backbone.Collection.extend({
+		model: App.Models.Task
+	});
+	
+	App.Views.Tasks = Backbone.View.extend({
+		tagName: 'ul',
+		render: function(){
+			this.collection.each(this.addOne, this);
+			return this;
+			
+		},
+		addOne: function(task){
+			// создавать новый дочерний вид
+			var taskView = new App.Views.Task({ model:task });
+			// добавить его в корневой элемент
+			this.$el.append(taskView.render().el);
+		}
+	});
+	
+	 var tasksCollection = new App.Collections.Task([
+	    {
 		 title: 'Сходить в магазин',
 		 priority: 4
-	 });
-	 var taskView = new App.Views.Task({ model: task });
-	 
-	 
-	 console.log(taskView.render().el);
+	    },
+		
+		 {
+		 title: 'Поулчить почту',
+		 priority: 3
+	    },
+		
+		 {
+		 title: 'Сходить на работу',
+		 priority: 5
+	    }
+	]);
 	
+	var tasksView = new App.Views.Tasks({ collection: tasksCollection});
 	
+	tasksView.render();
+	$('body').html(tasksView.el);
 		
 });
